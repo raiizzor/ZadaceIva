e8ce56fa7756a30f3ccab77ec6c4553f34463548
raiizzor <iva.pavlovic2@gmail.com>
2015-11-01 14:18:53 +0100

    Druga i treca zadaca
    Rjesene druga i treca zadaca iz Pythona

---
+++ 2zadaca.py.py
@@ -0,0 +1,76 @@
+def main():
+    ime()
+    brojevi()
+    ime_prezime()
+
+def ime():
+    lokalna= "Iva"
+    print lokalna
+
+def brojevi():
+    print (3 + 7)
+
+def ime_prezime():
+    lokalna_1= "Pero "
+    lokalna_2= "Peric"
+    print (lokalna_1 + lokalna_2)
+
+ime()    
+
+def godine():
+    print 24
+
+
+main()
+
+def visina():
+    cm= 165
+    print cm
+
+
+def adresa():
+    broj= 45
+    ulica= " Ozaljska"
+    print (str(broj) + ulica)
+
+visina()    
+adresa()
+
+
+def main():
+    ispis_imena ("Iva")
+
+def ispis_imena(ime):
+    print ime
+
+main()
+
+
+def main():
+    ime_prezime ("Ivo " , "Peric")
+
+def ime_prezime(ime , prezime):
+    print (ime , prezime)
+
+main()
+
+
+def main():
+    potenciranje (3)
+
+def potenciranje (broj):
+    print broj**2
+
+main()
+
+
+def main():
+    ime_godine ("Iva " ,20)
+
+def ime_godine(ime , godine):
+    print (ime , godine)
+
+main()    
+    
+
+    
---
+++ 3zadaca.py.py
@@ -0,0 +1,122 @@
+def main():
+    ime(12)
+
+def ime(naziv):
+    print naziv
+
+main()    
+
+
+def main():
+    visina_sirina(12 , 24.5)
+
+def visina_sirina(visina , sirina):
+    print(visina , sirina)
+
+main()
+
+
+def main():
+    adresa(12 , " Vukovarska")
+
+def adresa(broj , ulica):
+    ulica= "Ozaljska"
+    print(broj , ulica)
+
+main()    
+
+
+ULICA= "Ozaljska"
+
+
+if 3>2:
+    print "3 je vece od 2"
+
+    
+prezime= "Pavlovic"
+
+if prezime== "Pavlovic":
+    print "Pozdrav Iva"
+
+
+odsjek= "Informacijske"
+
+if odsjek== "Germanistika":
+    print "Pozdrav Germanistika."
+else:
+    print "Pozdrav Informacijske."
+    
+
+
+ocjena= 4
+
+if ocjena >= 2:
+    print "Prosao si."
+else:
+    print "Nisi prosao ispit."
+
+
+temperatura= 22
+
+if temperatura >= 30:
+    print "Jako je vruce."
+elif temperatura == 22:
+    print "Savrseno."
+else:
+    print "Zahladnilo je."
+
+
+godine= 18
+
+if godine <= 16:
+    print "Ne smijes ici u klub."
+elif godine==17:
+    print "Nedostaje ti jedna godina."
+elif godine==18:
+    print "Imas dovoljno godina."
+else:
+    print "Imas i vise nego dovoljno godina."
+
+
+
+ime= "Iva"
+godine= 21
+
+if ime=="Marko":
+    print "Pozdrav Marko"
+elif ime=="Iva":
+    print "Savrseno" 
+    if godine>20:
+        print "Vise od 20 godina."
+    else:
+        print "Manje od 20 godina."
+else:
+    print "ERROR"
+
+
+
+boja= "crvena"
+intenzitet= 10
+
+if boja=="bijela":
+    print "Bijela boja"
+elif boja=="crvena":
+    print "Crvena boja"
+    if intenzitet>10:
+        print "Prejak intenzitet"
+    elif intenzitet==10:
+        print "Savrsen intenzitet"
+    else:
+        print "Intenzitet je premal"
+else:
+    print "ERROR"
+    
+    
+    
+   
+            
+            
+    
+    
+    
+    
