6424829b4889e0bb47eb21c7239404bae6712512
raiizzor <iva.pavlovic2@gmail.com>
2015-11-01 17:48:06 +0100

    Ispravak zadace
    Ispravak druge i trece zadace.

---
+++ 2_3zadaca.py
@@ -0,0 +1,31 @@
+def godine():
+    print 24
+
+godine()
+
+
+def main():
+    ime()
+    visina_sirina()
+    adresa()
+
+
+def ime(ime=12):
+    print ime
+     
+
+def visina_sirina(visina= 12 , sirina= 24.5):
+    print (visina , sirina)
+
+
+def adresa(broj= 12 , ulica= "Vukovarska"):
+    print (broj , ulica)
+
+main()
+
+
+ def adresa(broj= 12 , ulica= "Vukovarska"):
+    ULICA= "Ozaljska"
+    print (broj , ulica)
+
+adresa()    
